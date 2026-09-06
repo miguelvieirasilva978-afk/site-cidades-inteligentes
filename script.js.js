c31f5d2698ebbb75f25da68cdf096368c5db151d
miguelvieirasilva978-afk <miguel.vieira.silva978@gmail.com>
2026-09-06 17:02:41 -0300

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,4 @@
+document.getElementById('btnMensagem').addEventListener('click', function() {
+    const mensagemElemento = document.getElementById('mensagem');
+    mensagemElemento.innerText = "Dica: Cidades inteligentes reduzem o uso de energia usando iluminação pública em LED com sensores de movimento!";
+});
